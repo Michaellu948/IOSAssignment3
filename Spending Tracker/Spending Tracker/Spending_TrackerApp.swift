46ec7677592fce5fd5fd2786517bc4b62e3fa70f
Michaellu948 <126222432+Michaellu948@users.noreply.github.com>
2024-05-06 13:32:54 +1000

    Create Xcode project

---
+++ Spending Tracker/Spending Tracker/Spending_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spending_TrackerApp.swift
+//  Spending Tracker
+//
+//  Created by Michael Lu on 6/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct Spending_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
